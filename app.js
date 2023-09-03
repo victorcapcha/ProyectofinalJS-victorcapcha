c86045e6d5864ff905038cc77fd18bdcd211c19d
victor <victor_14x_02s@hotmail.com>
2023-09-02 23:24:05 -0300

    agregando codigo

--- app.js
+++ app.js
@@ -5,20 +5,37 @@ const SELECTOR_VACIAR_CARRITO = '#vaciar-carrito';
 const ATRIBUTO_DATA_ID = 'data-id';
 
 const carrito = document.querySelector(SELECTOR_CARRITO);
-const elementos1 = document.querySelector(SELECTOR_ELEMENTOS_1);
-const elementos2 = document.querySelector(SELECTOR_ELEMENTOS_2);
 const lista = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector(SELECTOR_VACIAR_CARRITO);
 let carritoItems = [];
 
-cargarEventListeners();
+document.addEventListener('DOMContentLoaded', () => {
+  cargarEventListeners();
+  cargarCarritoDesdeLocalStorage();
+  cargarCarritoDesdeJSON();
+});
 
 function cargarEventListeners() {
+  carrito.addEventListener('click', (e) => {
+    if (e.target.classList.contains('borrar')) {
+      eliminarElemento(e.target.getAttribute(ATRIBUTO_DATA_ID));
+    } else if (e.target.classList.contains('agregar')) {
+      agregarCantidad(e.target.getAttribute(ATRIBUTO_DATA_ID));
+    } else if (e.target.classList.contains('quitar')) {
+      quitarCantidad(e.target.getAttribute(ATRIBUTO_DATA_ID));
+    }
+  });
+
+  vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+  const elementos1 = document.querySelector(SELECTOR_ELEMENTOS_1);
+  const elementos2 = document.querySelector(SELECTOR_ELEMENTOS_2);
+
   elementos1.addEventListener('click', comprarElemento);
   elementos2.addEventListener('click', comprarElemento);
-  lista.addEventListener('click', eliminarElemento);
-  vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
-  document.addEventListener('DOMContentLoaded', cargarCarritoLocalStorage);
+
+  const formularioCompra = document.getElementById('datos-compra');
+  formularioCompra.addEventListener('submit', finalizarCompra);
 }
 
 function comprarElemento(e) {
@@ -33,63 +50,54 @@ function comprarElemento(e) {
 
 function leerDatosElemento(elemento) {
   return {
-      imagen: elemento.querySelector('img').src,
-      titulo: elemento.querySelector('h3').textContent,
-      precio: elemento.querySelector('.precio').textContent,
-      id: elemento.querySelector('a').getAttribute(ATRIBUTO_DATA_ID),
-      cantidad: 1
+    imagen: elemento.querySelector('img').src,
+    titulo: elemento.querySelector('h3').textContent,
+    precio: elemento.querySelector('.precio').textContent,
+    id: elemento.querySelector('a').getAttribute(ATRIBUTO_DATA_ID),
+    cantidad: 1,
   };
 }
 
-
 function insertarCarrito(elemento) {
-  const elementoExistente = carritoItems.find(item => item.id === elemento.id);
+  const elementoExistente = carritoItems.find((item) => item.id === elemento.id);
 
   if (elementoExistente) {
-      elementoExistente.cantidad++;
+    elementoExistente.cantidad++;
   } else {
-      carritoItems.push(elemento);
+    carritoItems.push(elemento);
   }
 
   actualizarCarrito();
   guardarCarritoLocalStorage();
 }
 
-function eliminarElemento(e) {
-  e.preventDefault();
-  if (e.target.classList.contains('borrar')) {
-    const elementoId = e.target.getAttribute(ATRIBUTO_DATA_ID);
-    const indice = carritoItems.findIndex(item => item.id === elementoId);
+function eliminarElemento(id) {
+  const indice = carritoItems.findIndex((item) => item.id === id);
 
-    if (indice !== -1) {
-      carritoItems.splice(indice, 1);
-      actualizarCarrito();
-      guardarCarritoLocalStorage();
-    }
+  if (indice !== -1) {
+    carritoItems.splice(indice, 1);
+    actualizarCarrito();
+    guardarCarritoLocalStorage();
   }
 }
 
-function agregarCantidad(e) {
-  const elementoId = e.target.getAttribute(ATRIBUTO_DATA_ID);
-  const elemento = carritoItems.find(item => item.id === elementoId);
+function agregarCantidad(id) {
+  const elemento = carritoItems.find((item) => item.id === id);
 
   if (elemento) {
-      elemento.cantidad++;
-      actualizarCarrito();
-      guardarCarritoLocalStorage();
+    elemento.cantidad++;
+    actualizarCarrito();
+    guardarCarritoLocalStorage();
   }
 }
 
-function quitarCantidad(e) {
-  const elementoId = e.target.getAttribute(ATRIBUTO_DATA_ID);
-  const elemento = carritoItems.find(item => item.id === elementoId);
+function quitarCantidad(id) {
+  const elemento = carritoItems.find((item) => item.id === id);
 
-  if (elemento) {
-      if (elemento.cantidad > 1) {
-          elemento.cantidad--;
-          actualizarCarrito();
-          guardarCarritoLocalStorage();
-      }
+  if (elemento && elemento.cantidad > 1) {
+    elemento.cantidad--;
+    actualizarCarrito();
+    guardarCarritoLocalStorage();
   }
 }
 
@@ -102,55 +110,87 @@ function vaciarCarrito() {
 function actualizarCarrito() {
   lista.innerHTML = '';
 
-  carritoItems.forEach(elemento => {
-      const row = document.createElement('tr');
-      row.innerHTML = `
-          <td>
-              <img src="${elemento.imagen}" width="100">
-          </td>
-          <td>
-              ${elemento.titulo}
-          </td>
-          <td>
-              ${elemento.precio}
-          </td>
-          <td>
-              <button class="quitar" data-id="${elemento.id}">-</button>
-              <span>${elemento.cantidad}</span>
-              <button class="agregar" data-id="${elemento.id}">+</button>
-          </td>
-          <td>
-              <a href="#" class="borrar" data-id="${elemento.id}">X</a>
-          </td>
-      `;
-      lista.appendChild(row);
+  carritoItems.forEach((elemento) => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>
+        <img src="${elemento.imagen}" width="100">
+      </td>
+      <td>
+        ${elemento.titulo}
+      </td>
+      <td>
+        ${elemento.precio}
+      </td>
+      <td>
+        <button class="quitar" data-id="${elemento.id}">-</button>
+        <span>${elemento.cantidad}</span>
+        <button class="agregar" data-id="${elemento.id}">+</button>
+      </td>
+      <td>
+        <a href="#" class="borrar" data-id="${elemento.id}">X</a>
+      </td>
+    `;
+    lista.appendChild(row);
   });
 
   const botonesAgregar = document.querySelectorAll('.agregar');
   const botonesQuitar = document.querySelectorAll('.quitar');
 
-  botonesAgregar.forEach(boton => {
-      boton.addEventListener('click', agregarCantidad);
+  botonesAgregar.forEach((boton) => {
+    boton.addEventListener('click', () => agregarCantidad(boton.getAttribute(ATRIBUTO_DATA_ID)));
   });
 
-  botonesQuitar.forEach(boton => {
-      boton.addEventListener('click', quitarCantidad);
+  botonesQuitar.forEach((boton) => {
+    boton.addEventListener('click', () => quitarCantidad(boton.getAttribute(ATRIBUTO_DATA_ID)));
   });
 }
-
-
 
 function guardarCarritoLocalStorage() {
   localStorage.setItem('carrito', JSON.stringify(carritoItems));
 }
 
-function cargarCarritoLocalStorage() {
-  const carritoLocalStorage = JSON.parse(localStorage.getItem('carrito'));
-
-  if (carritoLocalStorage) {
-    carritoItems = carritoLocalStorage;
+function cargarCarritoDesdeLocalStorage() {
+  const carritoGuardado = localStorage.getItem('carrito');
+  if (carritoGuardado && carritoItems.length === 0) {
+    carritoItems = JSON.parse(carritoGuardado);
     actualizarCarrito();
   }
 }
 
+function cargarCarritoDesdeJSON() {
+  fetch('productos.json')
+    then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
 
+      return response.json();
+    })
+    then((data) => {
+      carritoItems = data;
+      actualizarCarrito();
+    })
+    .catch((error) => {
+      console.error('Error al cargar los datos desde el JSON:', error);
+    });
+}
+
+function finalizarCompra(e) {
+  e.preventDefault();
+
+  const nombre = document.getElementById('nombre').value;
+  const direccion = document.getElementById('direccion').value;
+  const telefono = document.getElementById('telefono').value;
+
+  if (!nombre || !direccion || !telefono) {
+    alert('Por favor, completa todos los campos.');
+    return;
+  }
+
+  vaciarCarrito();
+  const formularioCompra = document.getElementById('datos-compra');
+  formularioCompra.style.display = 'none';
+  const mensajeCompra = document.getElementById('mensaje-compra');
+  mensajeCompra.style.display = 'block';
+}
